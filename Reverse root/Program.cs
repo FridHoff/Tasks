896d5279aa324b3eb99b245873fad5b3f0f39475
FridHoff <flex210879@mail.ru>
2023-11-10 21:04:52 +0300

    Добавьте файлы проекта.

---
+++ Reverse root/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    static void Main(string[] args)
+    {
+        NumberFormatInfo info = NumberFormatInfo.InvariantInfo;
+        string[] numbers = Console.In.ReadToEnd().Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        for (int i = numbers.Length - 1; i >= 0; i--)
+        {
+            double root = Math.Sqrt(double.Parse(numbers[i], info));
+            Console.WriteLine(string.Format(info, "{0:F4}", root));
+        }
+    }
+}
